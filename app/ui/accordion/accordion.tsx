6d7d6014c924818cc32b3944dfbd78674b0ca333
Konstantin Fadeev <kostya.fad1@gmail.com>
2023-11-11 21:07:26 +0500

    fix CustomCheckbox

--- app/ui/accordion/accordion.tsx
+++ app/ui/accordion/accordion.tsx
@@ -2,10 +2,9 @@
 
 import React from "react";
 import {Accordion, AccordionItem} from "@nextui-org/react";
-import {array} from "zod";
 
 
-export default function AccordionMain({data}: { data: array }) {
+export default function AccordionMain({ data }: { data: any }) {
 
     return (
         <Accordion
