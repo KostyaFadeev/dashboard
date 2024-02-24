a0b91b5755f496d43a39f5a83627ae42356ebdd0
Konstantin Fadeev <kostya.fad1@gmail.com>
2024-02-24 13:00:20 +0500

    fix card info

--- app/cart/CheckoutLink.tsx
+++ app/cart/CheckoutLink.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import {Button} from "@nextui-org/react";
 import React from "react";
 import {deleteAll} from "@/app/lib/cookies";
 import NextLink from "next/link";
+import {ChatBubbleLeftRightIcon} from "@heroicons/react/24/outline";
 
 
 type Props = {
@@ -27,19 +27,32 @@ export const CheckoutLink = ({disabled}: Props) => {
     }
 
     return (
-        <NextLink
-			data-testid="CheckoutLink"
-			onClick={(e) => disabled && e.preventDefault()}
-			aria-disabled={disabled}
-			href={`/checkout`}
-		>
-            <Button
-                onClick={handleClick}
-                radius="sm"
-                className="mt-auto h-12 w-80 bg-green-500 text-xl text-white shadow-lg"
-            >
-                Подтвердить заказ!
-            </Button>
-        </NextLink>
+        <div className="p-4 flex-colum border-2 rounded-lg border-blue-500">
+            <div className="flex items-center gap-4 mb-2">
+                <ChatBubbleLeftRightIcon width={24}/>
+                <div className="text-lg">Дорогой клиент!</div>
+            </div>
+            <div className="text-lg">
+                Этот раздел пока в разработке 👨‍💻 Совсем скоро вы сможете приобрести товары через наш официальный
+                сайт.
+                Сейчас заказать можно тут, через менеджеров <NextLink
+                href={'https://t.me/Boundary_delivery'}>https://t.me/Boundary_delivery</NextLink>.
+                Спасибо, что вы уже с нами 😇
+            </div>
+        </div>
+        // <NextLink
+        // 	data-testid="CheckoutLink"
+        // 	onClick={(e) => disabled && e.preventDefault()}
+        // 	aria-disabled={disabled}
+        // 	href={`/checkout`}
+        // >
+        //     <Button
+        //         onClick={handleClick}
+        //         radius="sm"
+        //         className="mt-auto h-12 w-80 bg-green-500 text-xl text-white shadow-lg"
+        //     >
+        //         Подтвердить заказ!
+        //     </Button>
+        // </NextLink>
     );
 };
